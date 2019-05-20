e8767b824693144d9fa0211fba47fdefd6256a46
Brendan Robinson <brendanr425@gmail.com>
2019-05-20 19:14:05 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,15 @@
 def unsafe?(speed)
-  return true if speed < 40  || speed > 60
+  if speed < 40 || speed > 60
+    return true
+  else
+    return false
+  end
 end
 
 
 
+
 def not_safe?(speed)
+
 
 end
